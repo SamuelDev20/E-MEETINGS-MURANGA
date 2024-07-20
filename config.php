4c5a2cbbf8719d9938e9e27c9844f7597b610633
SamuelDev20 <samwelwamwea32@gmail.com>
2024-07-20 13:04:07 +0300

    made changes to config.php

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
-$host = 'localhost';
-$db = 'county';
-$user = 'root';
-$pass = '';
+$host = 'dpg-cqdnvn08fa8c73doeul0-a.oregon-postgres.render.com';
+$db = 'njoki_samuel_wamwea_8kcq';
+$user = 'njoki_samuel_wamwea_8kcq_user';
+$pass = 'N4vzkBtf79vuuOkmoKFu6azDFbEcgdAK';
+$port = '5432'; // Default PostgreSQL port
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+    $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Could not connect to the database $db :" . $e->getMessage());
